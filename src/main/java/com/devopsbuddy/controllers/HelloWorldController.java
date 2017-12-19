930569e1142a26c79b1f12e6ef87c831da34575e
Chularansi Fernando <chularansifernando@yahoo.com>
2017-12-19 18:04:45 +0530

    Deleted Extra devopsbuddy folder from test package.

--- src/main/java/com/devopsbuddy/controllers/HelloWorldController.java
+++ src/main/java/com/devopsbuddy/controllers/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.devopsbuddy.devopsbuddy.controllers;
+package com.devopsbuddy.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
